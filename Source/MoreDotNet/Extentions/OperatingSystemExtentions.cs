e7e18feae99abcb6701eb81cb0ee63336fd471cc
Teodor92 <teodor.ivanov92@gmail.com>
2016-03-04 20:02:53 +0200

    Added OS and Boolean extention

---
+++ Source/MoreDotNet/Extentions/OperatingSystemExtentions.cs
@@ -0,0 +1,31 @@
+﻿namespace MoreDotNet.Extentions
+{
+    using System;
+
+    public static class OperatingSystemExtentions
+    {
+        public static bool IsWinXpOrHigher(this OperatingSystem os)
+        {
+            return (os.Platform == PlatformID.Win32NT)
+              && ((os.Version.Major > 5) || ((os.Version.Major == 5) && (os.Version.Minor >= 1)));
+        }
+
+        public static bool IsWinVistaOrHigher(this OperatingSystem os)
+        {
+            return (os.Platform == PlatformID.Win32NT)
+              && (os.Version.Major >= 6);
+        }
+
+        public static bool IsWin7OrHigher(this OperatingSystem os)
+        {
+            return (os.Platform == PlatformID.Win32NT)
+              && ((os.Version.Major > 6) || ((os.Version.Major == 6) && (os.Version.Minor >= 1)));
+        }
+
+        public static bool IsWin8OrHigher(this OperatingSystem os)
+        {
+            return (os.Platform == PlatformID.Win32NT)
+              && ((os.Version.Major > 6) || ((os.Version.Major == 6) && (os.Version.Minor >= 2)));
+        }
+    }
+}
