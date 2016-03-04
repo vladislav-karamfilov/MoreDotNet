6bffb9ec6f1eeed042d27445fa22fa87f9133aac
Teodor92 <teodor.ivanov92@gmail.com>
2016-03-04 17:58:50 +0200

    Added dictionary extentions

--- Source/MoreDotNet/Extentions/DictionaryExtentions.cs
+++ Source/MoreDotNet/Extentions/DictionaryExtentions.cs
@@ -1,6 +1,33 @@
 ﻿namespace MoreDotNet.Extentions
 {
+    using System.Collections.Generic;
+
     public static class DictionaryExtentions
     {
+        /// <summary>
+        /// If a key exists in a dictionary, return its value, 
+        /// otherwise return the default value for that type.
+        /// </summary>
+        public static TValue GetWithDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+        {
+            return dict.GetWithDefault(key, default(TValue));
+        }
+
+        /// <summary>
+        /// If a key exists in a dictionary, return its value,
+        /// otherwise return the provided default value.
+        /// </summary>
+        public static TValue GetWithDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
+        {
+            TValue outValue;
+            var hasValue = dict.TryGetValue(key, out outValue);
+
+            if (hasValue)
+            {
+                return outValue;
+            }
+
+            return defaultValue;
+        }
     }
 }
