ca8ddb786772d4d57bdcf747fe83f854db95964b
Teodor92 <teodor.ivanov92@gmail.com>
2016-03-06 23:10:31 +0200

    Added documentation to the dictionary extentions

--- Source/MoreDotNet/Extentions/Common/DictionaryExtentions.cs
+++ Source/MoreDotNet/Extentions/Common/DictionaryExtentions.cs
@@ -9,19 +9,34 @@
         /// If a key exists in a dictionary, return its value, 
         /// otherwise return the default value for that type.
         /// </summary>
-        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+        /// <typeparam name="TKey">The key type of the <see cref="IDictionary{TKey,TValue}"/></typeparam>
+        /// <typeparam name="TValue">The value type of the <see cref="IDictionary{TKey,TValue}"/></typeparam>
+        /// <param name="dictionary">The <see cref="IDictionary{TKey,TValue}"/> instance on which the extension method is called.</param>
+        /// <param name="key">The key we are searching for.</param>
+        /// <returns>
+        /// The associated value with the given <paramref name="key"/> or if the key does not exists - the default value of <see cref="TValue"/>
+        /// </returns>
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
         {
-            return dict.GetOrDefault(key, default(TValue));
+            return dictionary.GetOrDefault(key, default(TValue));
         }
 
         /// <summary>
         /// If a key exists in a dictionary, return its value,
         /// otherwise return the provided default value.
         /// </summary>
-        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
+        /// <typeparam name="TKey">The key type of the <see cref="IDictionary{TKey,TValue}"/></typeparam>
+        /// <typeparam name="TValue">The value type of the <see cref="IDictionary{TKey,TValue}"/></typeparam>
+        /// <param name="dictionary">The <see cref="IDictionary{TKey,TValue}"/> instance on which the extension method is called.</param>
+        /// <param name="key">The key we are searching for.</param>
+        /// <param name="defaultValue">A default value if the <paramref name="key"/> does not exist in the <paramref name="dictionary"/></param>
+        /// <returns>
+        /// The associated value with the given <paramref name="key"/> or if the key does not exists - <paramref name="defaultValue"/>
+        /// </returns>
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
         {
             TValue outValue;
-            var hasValue = dict.TryGetValue(key, out outValue);
+            var hasValue = dictionary.TryGetValue(key, out outValue);
 
             if (hasValue)
             {
