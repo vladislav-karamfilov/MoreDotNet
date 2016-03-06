8c188fe40489aa156b87944cc5fbab6707d8ae7d
Teodor92 <teodor.ivanov92@gmail.com>
2016-03-06 22:39:22 +0200

    Added tests for ICollection extention methods

---
+++ Source/MoreDotNet.Test/Extentions/CollectionExtentions/AddRangeTests.cs
@@ -0,0 +1,38 @@
+﻿namespace MoreDotNet.Tests.Extentions.CollectionExtentions
+{
+    using System;
+    using System.Collections.Generic;
+
+    using MoreDotNet.Extentions;
+
+    using Xunit;
+
+    public class AddRangeTests
+    {
+        [Fact]
+        public void AddRange_ItemsAsParams_ShouldBeAddedToCollection()
+        {
+            ICollection<int> expected = new HashSet<int> { 1, 2, 3, 4, 5 };
+            ICollection<int> input = new HashSet<int> { 1 };
+            input.AddRange(2, 3, 4, 5);
+            Assert.Equal(expected, input);
+        }
+
+        [Fact]
+        public void AddRange_ItemsAsIEnumerable_ShouldBeAddedToCollection()
+        {
+            ICollection<int> expected = new HashSet<int> { 1, 2, 3, 4, 5 };
+            ICollection<int> input = new HashSet<int> { 1 };
+            IEnumerable<int> itemsToAdd = new HashSet<int> { 2, 3, 4, 5 };
+            input.AddRange(itemsToAdd);
+            Assert.Equal(expected, input);
+        }
+
+        [Fact]
+        public void AddRange_NullItemsGiven_ShouldThrowException()
+        {
+            ICollection<int> input = new HashSet<int> { 1, 2, 3, 4, 5 };
+            Assert.Throws<ArgumentNullException>(() => input.AddRange(null));
+        }
+    }
+}
--- Source/MoreDotNet/Extentions/CollectionExtentions.cs
+++ Source/MoreDotNet/Extentions/CollectionExtentions.cs
@@ -5,14 +5,44 @@
 
     public static class CollectionExtentions
     {
-        public static void AddRange<T, TS>(this ICollection<T> list, params TS[] values) where TS : T
+        /// <summary>
+        /// Adds a range of values to an ICollection.
+        /// </summary>
+        /// <typeparam name="T">The element type of the ICollection.</typeparam>
+        /// <param name="items">The ICollection instance on which the extension method is called.</param>
+        /// <param name="values">The items we are adding to the ICollection.</param>
+        public static void AddRange<T>(this ICollection<T> items, params T[] values)
         {
+            values.ThrowIfArgumentIsNull("values");
+
             foreach (var value in values)
             {
-                list.Add(value);
+                items.Add(value);
             }
         }
 
+        /// <summary>
+        /// Adds a range of values to an ICollection.
+        /// </summary>
+        /// <typeparam name="T">The element type of the ICollection.</typeparam>
+        /// <param name="items">The ICollection instance on which the extension method is called.</param>
+        /// <param name="values">The items we are adding to the ICollection.</param>
+        public static void AddRange<T>(this ICollection<T> items, IEnumerable<T> values)
+        {
+            values.ThrowIfArgumentIsNull("values");
+
+            foreach (var value in values)
+            {
+                items.Add(value);
+            }
+        }
+
+        /// <summary>
+        /// Checks if a collections is null or empty.
+        /// </summary>
+        /// <typeparam name="T">The item type of the ICollection.</typeparam>
+        /// <param name="items">The ICollection instance on which the extension method is called.</param>
+        /// <returns>True if the collection is null or empty</returns>
         public static bool IsNullOrEmpty<T>(this ICollection<T> items)
         {
             return items == null || !items.Any();
