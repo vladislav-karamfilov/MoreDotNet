ec03289ea44e7da9245c2cf2c812d629c5b69fa7
Teodor92 <teodor.ivanov92@gmail.com>
2016-03-06 20:10:41 +0200

    Finished unit testing string extentions

--- Source/MoreDotNet.Test/Extentions/StringExtentions/RemoveLastCharacterTests.cs
+++ Source/MoreDotNet.Test/Extentions/StringExtentions/RemoveLastCharacterTests.cs
@@ -1,6 +1,34 @@
 ﻿namespace MoreDotNet.Tests.Extentions.StringExtentions
 {
+    using System;
+
+    using MoreDotNet.Extentions;
+
+    using Xunit;
+
     public class RemoveLastCharacterTests
     {
+        [Fact]
+        public void RemoveLastCharacter_ParseNullInput_ShouldThrowException()
+        {
+            string input = null;
+            Assert.Throws<ArgumentNullException>(() => input.RemoveLastCharacter());
+        }
+
+        [Fact]
+        public void RemoveLastCharacter_ParseEmptyString_ShouldThrowException()
+        {
+            string input = string.Empty;
+            Assert.Throws<ArgumentOutOfRangeException>(() => input.RemoveLastCharacter());
+        }
+
+        [Fact]
+        public void RemoveLastCharacter_ParseSampleString_ShouldReturnTrimmedString()
+        {
+            var expected = "Greetings";
+            var input = "Greetings!";
+            var actual = input.RemoveLastCharacter();
+            Assert.Equal(expected, actual);
+        }
     }
 }
