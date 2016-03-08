aea9d7a2b1e4b225e90abcd5b105dda6a2b7634e
Teodor92 <teodor.ivanov92@gmail.com>
2016-03-08 18:01:14 +0200

    Added unit tests for boolean extentions

---
+++ Source/MoreDotNet.Test/Extentions/Common/BooleanExtentions/WhenFalseTests.cs
@@ -0,0 +1,47 @@
+﻿namespace MoreDotNet.Tests.Extentions.Common.BooleanExtentions
+{
+    using MoreDotNet.Extentions.Common;
+
+    using Xunit;
+
+    public class WhenFalseTests
+    {
+        [Fact]
+        public void WhenFalse_ParseFalseValue_ShouldReturnContent()
+        {
+            var input = false;
+            var inputContent = "Hello Worlds!";
+            var expected = "Hello Worlds!";
+            var actual = input.WhenFalse(expected);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void WhenFalse_ParseTrueValue_ShouldReturnDefaultValueOfContent()
+        {
+            var input = true;
+            var inputContent = "Hello Worlds!";
+            var expected = default(string);
+            var actual = input.WhenFalse(expected);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void WhenFalse_ParseFalseValue_ShouldExecuteAction()
+        {
+            var input = false;
+            var expected = "Hello Worlds!";
+            var actual = input.WhenFalse(() => "Hello Worlds!");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void WhenFalse_ParseTrueValue_ShouldNotExecuteAction()
+        {
+            var input = true;
+            var expected = default(string);
+            var actual = input.WhenFalse(() => "Hello Worlds!");
+            Assert.Equal(expected, actual);
+        }
+    }
+}
