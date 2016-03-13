90ea5bae797cd5945e1368f3574f042b54243005
Teodor92 <teodor.ivanov92@gmail.com>
2016-03-13 13:19:23 +0200

    Documented the stream extension methods.

--- Source/MoreDotNet/Extentions/Common/StreamExtentions.cs
+++ Source/MoreDotNet/Extentions/Common/StreamExtentions.cs
@@ -4,6 +4,11 @@
 
     public static class StreamExtensions
     {
+        /// <summary>
+        /// Converts a <see cref="Stream"/> to a byte array.
+        /// </summary>
+        /// <param name="input">The <see cref="Stream"/> being converted.</param>
+        /// <returns>A byte array representing a <see cref="Stream"/> contents.</returns>
         public static byte[] ToByteArray(this Stream input)
         {
             var buffer = new byte[16 * 1024];
@@ -19,6 +24,11 @@
             }
         }
 
+        /// <summary>
+        /// Converts a byte array to a <see cref="Stream"/>
+        /// </summary>
+        /// <param name="input">The byte array being converted.</param>
+        /// <returns>A <see cref="Stream"/> representing the contents of a byte array.</returns>
         public static Stream ToStream(this byte[] input)
         {
             var fileStream = new MemoryStream(input) { Position = 0 };
