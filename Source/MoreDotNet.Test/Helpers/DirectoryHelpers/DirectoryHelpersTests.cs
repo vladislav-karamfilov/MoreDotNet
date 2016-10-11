bc370debb813e3932c3b646f0b6d76d60f675d3a
leonpw <leonpw@outlook.com>
2016-10-11 11:09:51 +0200

    Cover DirectoryHelpers with unit tests #16

---
+++ Source/MoreDotNet.Test/Helpers/DirectoryHelpers/DirectoryHelpersTests.cs
@@ -0,0 +1,16 @@
+﻿namespace MoreDotNet.Tests.Helpers.DirectoryHelpers
+{
+    using System.IO;
+    using MoreDotNet.Helpers;
+    using Xunit;
+
+    public class DirectoryHelpersTests
+    {
+        [Fact]
+        public void CreateTempDirectory_ShouldCreateTempDirectory()
+        {
+            string path = DirectoryHelpers.CreateTempDirectory();
+            Assert.True(Directory.Exists(path));
+        }
+    }
+}
