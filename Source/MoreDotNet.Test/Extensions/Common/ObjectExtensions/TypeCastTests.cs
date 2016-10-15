8fd144756cff5238ad2092312ae6a10fa2c8d016
Geir-Ove Bøe <goggenb@gmail.com>
2016-10-15 23:35:09 +0200

    Added ObjectExtension tests

---
+++ Source/MoreDotNet.Test/Extensions/Common/ObjectExtensions/TypeCastTests.cs
@@ -0,0 +1,56 @@
+﻿namespace MoreDotNet.Tests.Extensions.Common.ObjectExtensions
+{
+    using System;
+    using MoreDotNet.Extensions.Common;
+    using Xunit;
+
+    public class TypeCastTests
+    {
+        [Fact]
+        public void As_StringObjectAsString_ShouldReturnString()
+        {
+            object input = "Value";
+            var result = input.As<string>();
+            Assert.IsType<string>(result);
+        }
+
+        [Fact]
+        public void As_IntAsString_ShouldReturnNull()
+        {
+            object input = 3;
+            var result = input.As<string>();
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void ToDictionary_Null_ShouldThrowArgumentNullException()
+        {
+            Example input = null;
+            Assert.Throws<ArgumentNullException>(() => input.ToDictionary());
+        }
+
+        [Fact]
+        public void ToDictionary_ExampleA_ShouldReturnDictionaryWithKeysAndValues()
+        {
+            var input = new Example
+            {
+                One = "First",
+                Two = "Second"
+            };
+            var result = input.ToDictionary();
+
+            Assert.True(result.ContainsKey("One"));
+            Assert.Equal("First", result["One"]);
+
+            Assert.True(result.ContainsKey("Two"));
+            Assert.Equal("Second", result["Two"]);
+        }
+
+        private class Example
+        {
+            public string One { get; set; }
+
+            public string Two { get; set; }
+        }
+    }
+}
---
+++ Source/MoreDotNet.Test/Extensions/Common/ObjectExtensions/TypeCheckTests.cs
@@ -0,0 +1,52 @@
+﻿namespace MoreDotNet.Tests.Extensions.Common.ObjectExtensions
+{
+    using System;
+    using MoreDotNet.Extensions.Common;
+    using Xunit;
+
+    public class TypeCheckTests
+    {
+        [Fact]
+        public void Is_AIsA_ShouldReturnTrue()
+        {
+            var input = new ExampleA();
+            Assert.True(input.Is<ExampleA>());
+        }
+
+        [Fact]
+        public void Is_AIsB_ShouldReturnFalse()
+        {
+            var input = new ExampleA();
+            Assert.False(input.Is<ExampleB>());
+        }
+
+        [Fact]
+        public void IsNot_NullItem_ShouldThrowShouldThrowArgumentNullException()
+        {
+            ExampleA input = null;
+            Assert.Throws<ArgumentNullException>(() => input.IsNot<ExampleB>());
+        }
+
+        [Fact]
+        public void Is_BIsNotA_ShouldReturnTrue()
+        {
+            var input = new ExampleB();
+            Assert.True(input.IsNot<ExampleA>());
+        }
+
+        [Fact]
+        public void Is_BIsNotB_ShouldReturnFalse()
+        {
+            var input = new ExampleB();
+            Assert.False(input.IsNot<ExampleB>());
+        }
+
+        private class ExampleA
+        {
+        }
+
+        private class ExampleB
+        {
+        }
+    }
+}
