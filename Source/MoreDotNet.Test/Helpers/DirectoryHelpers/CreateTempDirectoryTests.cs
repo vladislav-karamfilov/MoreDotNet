f8097cdc0b01d76f26639657c668b63eee1a1fb6
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2016-10-22 21:15:58 +0300

    DirectoryHelpers.CreateTempDirectory tests implemented with Smocks.

---
+++ Source/MoreDotNet.Test/Helpers/DirectoryHelpers/CreateTempDirectoryTests.cs
@@ -0,0 +1,37 @@
+﻿namespace MoreDotNet.Tests.Helpers.DirectoryHelpers
+{
+    using System.IO;
+
+    using MoreDotNet.Helpers;
+
+    using Smocks;
+    using Smocks.Matching;
+
+    using Xunit;
+
+    public class CreateTempDirectoryTests
+    {
+        [Fact]
+        public void CreateTempDirectory_ShouldCreateTempDirectory()
+        {
+            Smock.Run(context =>
+            {
+                // Arrange
+                var directoryCreated = false;
+                context.Setup(() => Directory.Exists(It.IsAny<string>())).Returns(false);
+                context.Setup(() => Directory.CreateDirectory(It.IsAny<string>())).Callback((string filePath) =>
+                {
+                    directoryCreated = true;
+                });
+
+                // Act
+                var path = DirectoryHelpers.CreateTempDirectory();
+
+                // Assert
+                Assert.True(directoryCreated);
+                Assert.NotNull(path);
+                Assert.StartsWith(Path.GetTempPath(), path);
+            });
+        }
+    }
+}
