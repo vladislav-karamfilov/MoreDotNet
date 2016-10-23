7a7f6404e457c6030dbc4a2f7ab0186a168a956a
georgimanov <georgimanov@gmail.com>
2016-10-23 21:17:17 +0300

    added next datetime test

--- Source/MoreDotNet.Test/Extensions/Common/RandomExtensions/NextDateTime.cs
+++ Source/MoreDotNet.Test/Extensions/Common/RandomExtensions/NextDateTime.cs
@@ -1,9 +1,9 @@
 ﻿namespace MoreDotNet.Tests.Extensions.Common.RandomExtensions
 {
     using System;
+    using System.Linq;
 
     using MoreDotNet.Extensions.Common;
-
     using Xunit;
 
     public class NextDateTime
@@ -15,6 +15,20 @@
             var result = random.NextDateTime();
 
             Assert.IsType<DateTime>(result);
+        }
+
+        [Fact]
+        public void NextDateTime_ShouldReturnDateTime_NewYear()
+        {
+            var random = new Random();
+            var counter = 100;
+            var dates = new DateTime[counter];
+            for (int i = 0; i < counter; i++)
+            {
+                dates[i] = random.NextDateTime();
+            }
+
+            Assert.Equal(counter, dates.Distinct().Count());
         }
     }
 }
