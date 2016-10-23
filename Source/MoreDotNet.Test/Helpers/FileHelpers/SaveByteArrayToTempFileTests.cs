0673c57c911249a63fb2d6658a5f1aaea4bc4e02
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2016-10-23 18:20:21 +0300

    Implemented tests for FileHelpers methods.

---
+++ Source/MoreDotNet.Test/Helpers/FileHelpers/SaveByteArrayToTempFileTests.cs
@@ -0,0 +1,56 @@
+﻿namespace MoreDotNet.Tests.Helpers.FileHelpers
+{
+    using System;
+    using System.IO;
+    using System.IO.Abstractions.TestingHelpers;
+
+    using MoreDotNet.Helpers;
+
+    using Smocks;
+    using Smocks.Matching;
+
+    using Xunit;
+
+    public class SaveByteArrayToTempFileTests
+    {
+        private static readonly Random Random = new Random();
+
+        [Fact]
+        public void SaveByteArrayToTempFile_NullBytesToWrite_ShouldThrow()
+        {
+            // Arrange
+            byte[] data = null;
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => FileHelpers.SaveByteArrayToTempFile(data));
+        }
+
+        [Theory]
+        [InlineData(new byte[] { })]
+        [InlineData(new byte[] { 1 })]
+        [InlineData(new byte[] { 1, 0 })]
+        [InlineData(new byte[] { 1, 0, 241, 222, 123 })]
+        [InlineData(new byte[] { 1, 0, 124, 225, 165, 229, 11, 69, 0, 1 })]
+        public void SaveByteArrayToTempFile_NonNullBytesToWrite_ShouldWriteCorrectDataToFile(byte[] bytesToWrite)
+        {
+            Smock.Run(context =>
+            {
+                // Arrange
+                var fileSystem = new MockFileSystem();
+
+                context.Setup(() => Path.GetTempFileName()).Returns(() => $@"C:\file-{Random.Next()}.tmp");
+                context
+                    .Setup(() => File.WriteAllBytes(It.IsAny<string>(), It.IsAny<byte[]>()))
+                    .Callback<string, byte[]>((filePath, contents) => fileSystem.File.WriteAllBytes(filePath, contents));
+
+                // Act
+                var path = FileHelpers.SaveByteArrayToTempFile(bytesToWrite);
+
+                // Assert
+                Assert.True(fileSystem.File.Exists(path));
+                Assert.Equal(bytesToWrite, fileSystem.File.ReadAllBytes(path));
+            });
+        }
+
+    }
+}
---
+++ Source/MoreDotNet.Test/Helpers/FileHelpers/SaveStringToTempFileTests.cs
@@ -0,0 +1,44 @@
+﻿namespace MoreDotNet.Tests.Helpers.FileHelpers
+{
+    using System;
+    using System.IO;
+    using System.IO.Abstractions.TestingHelpers;
+
+    using MoreDotNet.Helpers;
+
+    using Smocks;
+    using Smocks.Matching;
+
+    using Xunit;
+
+    public class SaveStringToTempFileTests
+    {
+        private static readonly Random Random = new Random();
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("File content")]
+        [InlineData("File\n \tcontent \b")]
+        public void SaveStringToTempFile_StringToWrite_ShouldWriteCorrectDataToFile(string stringToWrite)
+        {
+            Smock.Run(context =>
+            {
+                // Arrange
+                var fileSystem = new MockFileSystem();
+
+                context.Setup(() => Path.GetTempFileName()).Returns(() => $@"C:\file-{Random.Next()}.tmp");
+                context
+                    .Setup(() => File.WriteAllText(It.IsAny<string>(), It.IsAny<string>()))
+                    .Callback<string, string>((filePath, contents) => fileSystem.File.WriteAllText(filePath, contents));
+
+                // Act
+                var path = FileHelpers.SaveStringToTempFile(stringToWrite);
+
+                // Assert
+                Assert.True(fileSystem.File.Exists(path));
+                Assert.Equal(stringToWrite ?? string.Empty, fileSystem.File.ReadAllText(path));
+            });
+        }
+    }
+}
