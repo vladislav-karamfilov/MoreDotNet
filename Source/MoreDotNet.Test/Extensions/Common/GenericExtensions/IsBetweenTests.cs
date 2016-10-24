ef85cc40a9fc6dbda97342a23afeeb130084d533
Mikhail Ivchenko <ematirov@gmail.com>
2016-10-24 23:08:31 +0400

    Add IsBetween tests.

---
+++ Source/MoreDotNet.Test/Extensions/Common/GenericExtensions/IsBetweenTests.cs
@@ -0,0 +1,71 @@
+﻿namespace MoreDotNet.Tests.Extensions.Common.GenericExtensions
+{
+    using System;
+    using Moq;
+    using MoreDotNet.Extensions.Common;
+    using Xunit;
+
+    public class IsBetweenTests
+    {
+        private const int IsBigger = 1;
+        private const int IsEqual = 0;
+        private const int IsSmaller = -1;
+        private readonly Mock<IComparableWithSelf> actualMock = new Mock<IComparableWithSelf>();
+        private readonly Mock<IComparableWithSelf> lowerBoundMock = new Mock<IComparableWithSelf>();
+        private readonly Mock<IComparableWithSelf> upperBoundMock = new Mock<IComparableWithSelf>();
+
+        public interface IComparableWithSelf : IComparable<IComparableWithSelf>
+        {
+        }
+
+        [Fact]
+        public void WhenInRange_ShouldReturnTrue()
+        {
+            this.actualMock.Setup(x => x.CompareTo(this.lowerBoundMock.Object)).Returns(IsBigger);
+            this.actualMock.Setup(x => x.CompareTo(this.upperBoundMock.Object)).Returns(IsSmaller);
+
+            var actual = this.actualMock.Object.IsBetween(this.lowerBoundMock.Object, this.upperBoundMock.Object);
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void WhenSmallerThanLowerBound_ShouldReturnFalse()
+        {
+            this.actualMock.Setup(x => x.CompareTo(this.lowerBoundMock.Object)).Returns(IsSmaller);
+            this.actualMock.Setup(x => x.CompareTo(this.upperBoundMock.Object)).Returns(IsSmaller);
+
+            var actual = this.actualMock.Object.IsBetween(this.lowerBoundMock.Object, this.upperBoundMock.Object);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void WhenBiggerThanUpperBound_ShouldReturnFalse()
+        {
+            this.actualMock.Setup(x => x.CompareTo(this.lowerBoundMock.Object)).Returns(IsBigger);
+            this.actualMock.Setup(x => x.CompareTo(this.upperBoundMock.Object)).Returns(IsBigger);
+
+            var actual = this.actualMock.Object.IsBetween(this.lowerBoundMock.Object, this.upperBoundMock.Object);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void WhenExactlyUpperBound_ShouldReturnFalse()
+        {
+            this.actualMock.Setup(x => x.CompareTo(this.lowerBoundMock.Object)).Returns(IsBigger);
+            this.actualMock.Setup(x => x.CompareTo(this.upperBoundMock.Object)).Returns(IsEqual);
+
+            var actual = this.actualMock.Object.IsBetween(this.lowerBoundMock.Object, this.upperBoundMock.Object);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void WhenExactlyLowerBound_ShouldReturnTrue()
+        {
+            this.actualMock.Setup(x => x.CompareTo(this.lowerBoundMock.Object)).Returns(IsEqual);
+            this.actualMock.Setup(x => x.CompareTo(this.upperBoundMock.Object)).Returns(IsSmaller);
+
+            var actual = this.actualMock.Object.IsBetween(this.lowerBoundMock.Object, this.upperBoundMock.Object);
+            Assert.True(actual);
+        }
+    }
+}
