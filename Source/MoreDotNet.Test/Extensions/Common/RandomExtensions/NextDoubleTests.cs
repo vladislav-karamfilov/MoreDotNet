6f638e91690e0f4e22a33349262cb5e370472086
georgimanov <georgimanov@gmail.com>
2016-10-29 18:14:30 +0300

    fixed NextDoubleTests

--- Source/MoreDotNet.Test/Extensions/Common/RandomExtensions/NextDoubleTests.cs
+++ Source/MoreDotNet.Test/Extensions/Common/RandomExtensions/NextDoubleTests.cs
@@ -9,49 +9,83 @@
 
     public class NextDoubleTests
     {
-        [Fact]
-        public void NextDouble_ShouldReturnDouble()
-        {
-            var random = new Random();
-            var result = random.NextDouble(0, 10);
+        private const int Counter = 1000;
+        private const double DoubleResult = 0.15;
+        private const double MinValue = -50;
+        private const double MaxValue = 150;
 
-            Assert.IsType<double>(result);
+        [Fact]
+        public void NextDouble_RandomIsNull_ShouldThrow_NullReferenceException()
+        {
+            Random random = null;
+
+            Assert.Throws<NullReferenceException>(() => random.NextDouble());
         }
 
         [Fact]
-        public void NextDouble_ShouldReturnDouble_BetweenZeroAndTen()
+        public void NextDouble_ShouldReturnDouble_BetweenMinAndMaxValue()
         {
             var random = new Random();
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < Counter; i++)
             {
-                var result = random.NextDouble(0, 10);
+                var result = random.NextDouble(MinValue, MaxValue);
 
-                Assert.True(result >= 0 && result < 10);
+                Assert.True(result >= MinValue && result < MaxValue);
             }
         }
 
         [Fact]
-        public void NextDouble_ShouldReturnDouble_Zero()
+        public void NextDouble_MinValueIsGreaterThanMaxValue_ShouldThrow_ArgumentException()
         {
-            var randomMock = new Mock<Random>();
-            randomMock.Setup(r => r.NextDouble()).Returns(0);
-            var result = randomMock.Object.NextDouble(0, double.MaxValue);
+            var random = new Random();
 
-            Assert.Equal(0, result);
+            Assert.Throws<ArgumentException>(() => random.NextDouble(minValue: MaxValue, maxValue: MinValue));
         }
 
         [Fact]
-        public void NextDouble_ShouldReturnDouble_PredifinedValue()
+        public void NextDouble_MockedRandom_ShouldReturn_PredifinedValue()
         {
-            var mockedValue = 0.15;
-            var minValue = 0;
-            var maxValue = 30;
-
             var randomMock = new Mock<Random>();
-            randomMock.Setup(r => r.NextDouble()).Returns(mockedValue);
-            var result = randomMock.Object.NextDouble(minValue, maxValue);
+            randomMock.Setup(r => r.NextDouble()).Returns(DoubleResult);
+            var actualResult = randomMock.Object.NextDouble(MinValue, MaxValue);
+            var expectedResult = MinValue + (DoubleResult * (MaxValue - MinValue));
 
-            Assert.Equal(mockedValue * maxValue, result);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData(double.MinValue, double.MaxValue)]
+        [InlineData(MinValue, MaxValue)]
+        public void NextDouble_ShoulReturn_ValidResults(double min, double max)
+        {
+            var random = new Random();
+            var result = random.NextDouble(min, max);
+
+            Assert.True(result >= min && result < max);
+        }
+
+        [Theory]
+        [InlineData(double.NaN, MaxValue)]
+        [InlineData(MinValue, double.NaN)]
+        [InlineData(double.NaN, double.NaN)]
+        [InlineData(double.NegativeInfinity, double.PositiveInfinity)]
+        [InlineData(double.NegativeInfinity, MaxValue)]
+        public void NextDouble_InfinityOrNanValuesParams_ShoulReturn_NaNValues(double min, double max)
+        {
+            var random = new Random();
+            var result = random.NextDouble(min, max);
+
+            Assert.Equal(double.NaN, result);
+        }
+
+        [Theory]
+        [InlineData(MinValue, double.PositiveInfinity)]
+        public void NextDouble_PositiveInfinity_ShoulReturn_PositiveInfinity(double min, double max)
+        {
+            var random = new Random();
+            var result = random.NextDouble(min, max);
+
+            Assert.Equal(double.PositiveInfinity, result);
         }
     }
 }
