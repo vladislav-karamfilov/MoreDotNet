bcc1303c8c8bf8e724f08344cc09896acb5962f8
georgimanov <georgimanov@gmail.com>
2016-10-30 20:55:56 +0200

    extenden NextBoolTests

--- Source/MoreDotNet.Test/Extensions/Common/RandomExtensions/NextBoolTests.cs
+++ Source/MoreDotNet.Test/Extensions/Common/RandomExtensions/NextBoolTests.cs
@@ -8,32 +8,48 @@ namespace MoreDotNet.Tests.Extensions.Common.RandomExtensions
 
     public class NextBoolTests
     {
-        private const int Counter = 10;
+        private const int NumberOfTests = 10;
 
         [Fact]
-        public void NextBool_ShouldHThrow_NullReferenceException()
+        public void NextBool_RandomIsNull_ShouldThrowArgumentNullException()
         {
             Random random = null;
 
-            Assert.Throws<NullReferenceException>(() => random.Next());
+            Assert.Throws<ArgumentNullException>(() => random.NextBool());
         }
 
-        [Fact]
-        public void NextBool_ShouldReturn_True()
+        [Theory]
+        [InlineData(double.NegativeInfinity)]
+        [InlineData(double.NaN)]
+        [InlineData(-NumberOfTests)]
+        [InlineData(0)]
+        public void NextBool_ArgumentValueNotPositive_ShouldReturnFalse(double param)
         {
             var random = new Random();
-            var result = random.NextBool(1);
+            var result = random.NextBool(param);
+
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(double.PositiveInfinity)]
+        [InlineData(NumberOfTests)]
+        [InlineData(1)]
+        public void NextBool_ArgumentValuePositive_ShouldReturnTrue(double param)
+        {
+            var random = new Random();
+            var result = random.NextBool(param);
 
             Assert.True(result);
         }
 
         [Fact]
-        public void NextBool_ShouldReturn_TrueOrFalse_AtLeastOnce()
+        public void NextBool_IteratesGenerations_ShouldReturnTrueOrFalseAtLeastOnce()
         {
             var random = new Random();
             int returnedTrueAsResult = 0;
             int returnedFalseAsResult = 0;
-            for (int i = 0; i < Counter; i++)
+            for (int i = 0; i < NumberOfTests; i++)
             {
                 if (random.NextBool())
                 {
@@ -47,15 +63,6 @@ namespace MoreDotNet.Tests.Extensions.Common.RandomExtensions
 
             Assert.True(returnedTrueAsResult > 0);
             Assert.True(returnedFalseAsResult > 0);
-        }
-
-        [Fact]
-        public void NextBool_ShouldReturn_False()
-        {
-            var random = new Random();
-            var result = random.NextBool(0);
-
-            Assert.False(result);
         }
     }
 }
